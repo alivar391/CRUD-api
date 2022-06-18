7e9010e6d0256de480180f179933482c85c1dab9
alivar391 <patriot391@mail.ru>
2022-06-18 23:30:28 +0300

    feat: edit url check

--- src/index.ts
+++ src/index.ts
@@ -12,22 +12,21 @@ const server = http.createServer((req, res) => {
   if (req.url === "/api/users" && req.method === "GET") {
     getUsers(req, res);
   } else if (
-    req?.url?.match(/\/api\/users\/([0-9]+)/) &&
+    req?.url?.match(/^\/api\/users\/[\w-]+$/) &&
     req.method === "GET"
   ) {
     const id = req.url.split("/")[3];
-    console.log(req.url.split("/"), id);
     getUser(req, res, id);
   } else if (req.url === "/api/users" && req.method === "POST") {
     createUser(req, res);
   } else if (
-    req?.url?.match(/\/api\/users\/([0-9]+)/) &&
+    req?.url?.match(/^\/api\/users\/[\w-]+$/) &&
     req.method === "PUT"
   ) {
     const id = req.url.split("/")[3];
     updateUser(req, res, id);
   } else if (
-    req?.url?.match(/\/api\/users\/([0-9]+)/) &&
+    req?.url?.match(/^\/api\/users\/[\w-]+$/) &&
     req.method === "DELETE"
   ) {
     const id = req.url.split("/")[3];
